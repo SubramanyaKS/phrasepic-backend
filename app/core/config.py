244d50fdb6b2ef778e1460062f74fe34c09a7bbd
SubramanyaKS <subramanyaks2205@gmail.com>
2026-09-09 19:28:08 +0530

    changes which causes deployment to fail

--- app/core/config.py
+++ app/core/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     allowed_origins: list[str] = ["https://phrasepic.onrender.com"]
 
     class Config:
-        env_file = ".env",
-        env_file_encoding="utf-8",
-        extra="ignore",
+        env_file = ".env"
+        env_file_encoding="utf-8"
+        extra="ignore"
 
 
 settings = Settings()
